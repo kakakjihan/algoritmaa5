8a5d5ae80fc7accedd4250e9c553c1f1fed22e1e
kakakjihan <jihansahfitri6@gmail.com>
2024-10-06 22:45:22 +0700

    header fungsi

--- 25 Header function.php
+++ 25 Header function.php
@@ -1,5 +1,5 @@
 <?php
 //Header Location: Pengalihan Halaman
 header("Location: http://www.contoh.com/halaman_baru.php");
-exit; // Selalu panggil exit setelah header location untuk menghentikan eksekusi skrip
+exit; // Selalu panggill exit setelah header location untuk menghentikan eksekusi skrip
 ?>
