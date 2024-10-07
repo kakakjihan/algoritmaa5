35673c37db68e80e1c26d7b836cd0a8aee1ff964
kakakjihan <jihansahfitri6@gmail.com>
2024-10-07 10:30:10 +0700

    mengolah data formulir dengan php

--- 34 Mengolah Data Formulir dengan PHP.php
+++ 34 Mengolah Data Formulir dengan PHP.php
@@ -8,5 +8,5 @@ if(isset($_GET['query'])) {
 if(isset($_POST['username']) && isset($_POST['password'])) {
   $username = $_POST['username'];
   $password = $_POST['password'];
-  // Proses username dan password untuk autentikasi
+  // Prosess username dan password untuk autentikasi
 }
